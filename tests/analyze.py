fadc3854f0abb57d93851525f8eeb7d79944f076
Dunstan BECHT <dunstan@becht.network>
2021-08-14 01:13:29 +0200

    remove seed in analysis tests

--- tests/analyze.py
+++ tests/analyze.py
@@ -9,8 +9,8 @@ from lpa.input import analyze
 from lpa.input import sets
 from lpa.input.models import RDD
 
-d = sets.Distribution('circle', 1000, RDD, {'d': 1e15*1e-18, 'r': 0})
-s = sets.Sample(2, 'circle', 1000, RDD, {'d': 1e15*1e-18, 'r': 0})
+d = sets.Distribution('circle', 1000, RDD, {'d': 1e15*1e-18})
+s = sets.Sample(2, 'circle', 1000, RDD, {'d': 1e15*1e-18})
 
 # create a study range
 r, iK = analyze.intervals(100, 1000)
--- tests/parallel.py
+++ tests/parallel.py
@@ -11,8 +11,7 @@ from lpa.input import parallel
 from lpa.input import sets
 from lpa.input.models import RRDD
 
-r = {'v': 'E', 'f': 2, 's': 200, 'r': 0}
-s = sets.Sample(10000, 'circle', 1000, RRDD, r)
+s = sets.Sample(10000, 'circle', 1000, RRDD, {'v': 'E', 'f': 2, 's': 200})
 
 # export the pooled statistical analysis of the samples s of each core
 parallel.export(s)
