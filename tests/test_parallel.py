1773172e238418807526260c8410bff2df93467e
Dunstan BECHT <dunstan@becht.network>
2021-08-18 16:46:18 +0200

    update examples

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -10,10 +10,12 @@ sbatch test_parallel.job
 
 from lpa.input import parallel
 from test_sets import *
+import warnings
 
 d = sets.Distribution('square', 2000, *rdd)
 s = sets.Sample(625, 'circle', 1000, *rrdd, S=0+parallel.rank)
 
 # export the pooled statistical analysis of the samples s of each core
 parallel.export(d)
+warnings.filterwarnings("ignore") # read the warning in the module parallel
 parallel.export(s, exfmt='svg')
