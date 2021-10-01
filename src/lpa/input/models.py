f5c0268672ed45f0dca7ed0cf38c822c159070e8
Dunstan BECHT <dunstan@becht.network>
2021-10-01 10:11:22 +0200

    update RRDD and RCDD models

--- src/lpa/input/models.py
+++ src/lpa/input/models.py
@@ -4,14 +4,14 @@
 """
 Models for the random generation of dislocations.
 
-The following parameters can be used in the model parameters:
-    v: variant (RRDD, RCDD)
-    d: density (RDD, RRDD, RCDD)
-    s: side of the subarea (RRDD) or the cell (RCDD)
-    f: filling of the subareas (RRDD)
-    t: wall thickness (RCDD)
-    l: dipole length (RCDD)
-    name: parameters set nickname (RDD, RRDD, RCDD)
+The following fields can be used in the model parameters:
+    v: variant (in RRDD or RCDD)
+    d: density (in RDD, RRDD or RCDD)
+    s: side of the subarea (in RRDD) or the cell (for RCDD)
+    f: filling of the subareas (in RRDD or RCDD)
+    t: wall thickness (in RCDD)
+    l: dipole length (in RCDD)
+    name: parameters set nickname (in RDD, RRDD or RCDD)
 """
 
 import math
@@ -31,6 +31,12 @@ def RDD(
 
     "RDD" means: "Random Dislocation Distribution"
 
+    The positions of the dislocations are uniformly distributed in the
+    region of interest.
+
+    The number of dislocations with a positive Burger vector is equal
+    to the number of dislocations with a negative Burger vector.
+
     Input:
         g (str): geometry of the region of interest
         s (Scalar): size of the region of interest [nm]
@@ -48,21 +54,17 @@ def RDD(
     Complexity:
         O( r['d'] * a )
     """
-    # parameters
-    rd = r['d'] # density of dislocations
-    n = round(rd*a) # number of dislocations
+    nh = round(r['d']*a/2) # half of the total number of dislocations
+    nt = 2*nh # total number of dislocations
+    # dislocation Burgers vectors
+    b = np.concatenate((np.ones(nh), -np.ones(nh)))
     # dislocation positions
     if g == 'circle':
-        theta = 2*np.pi*G.random(n)
-        radius = s*np.sqrt(G.random(n))
-        p = np.stack((radius*np.cos(theta), radius*np.sin(theta)), axis=1)
+        phi = 2*np.pi*G.random(nt)
+        rad = s*np.sqrt(G.random(nt))
+        p = np.stack((rad*np.cos(phi), rad*np.sin(phi)), axis=1)
     elif g == 'square':
-        p = s*G.random([n, 2])
-    # dislocation Burgers vectors
-    bp = np.ones(n//2, dtype=int) # positive Burgers vectors
-    b = np.concatenate((bp, -bp)) # add negative Burgers vectors
-    if n%2 == 1: # add a random burger vector
-        b = np.concatenate((b, G.choice([1, -1], size=1)))
+        p = s*G.random([nt, 2])
     return p, b
 
 @beartype
@@ -82,10 +84,20 @@ def ticks(
     Output:
         t (ScalarList): ticks along an axis
 
+    Input example:
+        g = 'circle'
+        l = 3
+        s = 1
+
+    Output example:
+        t = np.array([-3, -2, -1, 0, 1, 2, 3])
+
     Complexity:
         O( l/s )
     """
     m = math.ceil(l/s)
+    if np.abs(m*s-l)/l>0.0001:
+        warnings.warn("the step is not a divisor of the length", Warning)
     if g == 'circle':
         return s*np.arange(-m, m+1) # [-2s, -s, 0, s, 2s] with m=2
     if g == 'square':
@@ -95,29 +107,33 @@ def ticks(
 def even_positions(
     t: ScalarList,
     f: int,
-) -> tuple:
+) -> ScalarListList:
     """
-    Return the positions with a number of points f in each case.
+    Return the grid case lower left corner positions.
 
     Input:
         t (ScalarList): ticks along an axis
         f (int): number of points in each case
 
     Output:
-        x (ScalarList): x coordinates of points
-        y (ScalarList): y coordinates of points
+        p (ScalarList): case lower left corner positions
 
     Input example:
         t = np.array([0, 1])
         f = 4
 
     Output example:
-        x = [0 0 0 0 1 1 1 1 0 0 0 0 1 1 1 1]
-        y = [0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1]
+        p = np.array([
+            [0, 0], [0, 0], [0, 0], [0, 0],
+            [1, 0], [1, 0], [1, 0], [1, 0],
+            [0, 1], [0, 1], [0, 1], [0, 1],
+            [1, 1], [1, 1], [1, 1], [1, 1],
+        ])
+
     """
     x = np.repeat(np.tile(t, len(t)), f)
     y = np.repeat(t, f*len(t))
-    return x, y
+    return np.stack((x,y), axis=1)
 
 @beartype
 def even_senses(
@@ -158,6 +174,16 @@ def RRDD(
 
     "RRDD" means: "Restrictedly Random Dislocation Distribution"
 
+    There is a fixed number of dislocations per subarea. These
+    dislocations are uniformly distributed within the subareas.
+
+    In the square geometry, the number of dislocations with a positive
+    Burger vector is equal to the number of dislocations with a
+    negative Burger vector. When the parameter 'v' is set to 'R', the
+    Burgers vectors are randomly distributed. When it is set to 'E',
+    the number of positive dislocations is equal to the number of
+    negative dislocations in each subarea.
+
     Input:
         g (str): geometry of the region of interest
         s (Scalar): size of the region of interest [nm]
@@ -176,35 +202,32 @@ def RRDD(
         'd' (Scalar): density of dislocations [nm^-2]
 
     The parameter 'd' is considered only if the parameter 'f' is not
-    specified. When the parameter 'v' is set to 'R', the Burgers
-    vectors are randomly distributed. When it is set to 'E', the
-    Burgers vectors are evenly distributed in each subarea.
+    specified.
 
     Complexity:
         O( r['f'] * s^2/r['s']^2 )
     """
-    # parameters
-    rv = r['v'] # model variant
-    rs = r['s'] # size of the subareas
     if 'f' in r:
-        rf = r['f'] # specified filling
+        f = r['f'] # specified filling
     else:
-        rf = round(r['d']*rs**2) # deduced filling
-    if rf%2 != 0:
-        raise ValueError('odd number of dislocations per subarea')
-    t = ticks(g, s, rs)[:-1] # left or bottom location of subareas
-    n = rf * len(t)**2 # number of dislocations
+        f = round(r['d']*r['s']**2) # deduced filling
+    if f%2 != 0:
+        raise ValueError("odd number of dislocations per subarea")
+    t = ticks(g, s, r['s'])[:-1] # left or bottom location of subareas
+    nt = f * len(t)**2 # total number of dislocations
+    nh = nt//2 # half of the total number of dislocations
     # dislocation positions
-    x, y = even_positions(t, rf)
-    p = np.stack((x,y), axis=1) + rs*G.random((n, 2))
+    p = even_positions(t, f) + r['s']*G.random((nt, 2))
     # dislocation Burgers vector
-    if rv == 'R':
-        b = G.choice([1, -1], size=n)
-    elif rv == 'E':
-        b = even_senses(t, rf)
+    if r['v'] == 'R':
+        b = G.permutation(np.concatenate((np.ones(nh), -np.ones(nh))))
+    elif r['v'] == 'E':
+        b = even_senses(t, f)
     # masking
-    m = geometries.mask(g, s, p)
-    return p[m], b[m]
+    if g == 'circle':
+        m = geometries.mask(g, s, p)
+        p, b = p[m], b[m]
+    return p, b
 
 @beartype
 def RCDD(
@@ -219,6 +242,18 @@ def RCDD(
 
     "RCDD" means: "Random Cell Dislocation Distribution"
 
+    There is a fixed number of dislocations per subarea. These
+    dislocations are uniformly distributed within the cell walls.
+
+    In the square geometry, the number of dislocations with a positive
+    Burger vector is equal to the number of dislocations with a
+    negative Burger vector. When the parameter 'v' is set to 'R', the
+    Burgers vectors are randomly distributed. When it is set to 'E',
+    the number of positive dislocations is equal to the number of
+    negative dislocations in each cell. When it is set to 'D', the
+    positive dislocations and negative dislocations are spaced by a
+    distance defined with the parameter 'l'.
+
     Input:
         g (str): geometry of the region of interest
         s (Scalar): size of the region of interest [nm]
@@ -232,78 +267,55 @@ def RCDD(
 
     The following parameters can be specified in r:
         'v' (str): variant ('R', 'E' or 'D')
-        'd' (Scalar): density of dislocations [nm^-2]
         's' (int): side of the subareas [nm]
         't' (int): thickness of the cell walls [nm]
         'l' (int): length of the dipoles [nm]
-
-    When the parameter 'v' is set to 'R', the positions and the Burgers
-    vectors senses are randomly distributed. When 'v' is set to 'E',
-    each cell contains a fixed number of dislocations and the Burgers
-    vectors senses are evenly distributed in each cell. When 'v' is set
-    to 'D', the Burgers vectors with positive sense and negative sense
-    are spaced by a distance defined with 'l'.
+        'f' (int): number of dislocations per subarea
+        'd' (Scalar): density of dislocations [nm^-2]
 
     Complexity:
         O( r['d'] * a )
     """
-    # parameters
-    rv = r['v'] # variant
-    rd = r['d'] # density of dislocations
-    rs = r['s'] # side of the cells
-    rt = r['t'] # thickness of the cell walls
-    if rt > rs/2:
+    if 'f' in r:
+        f = r['f'] # specified filling
+    else:
+        f = round(r['d']*r['s']**2) # deduced filling
+    f = 2*max(1, round(f/2))
+    if r['t'] > r['s']/2:
         raise ValueError("inconsistent thickness and cell side")
-    if rv == 'D':
-        rl = r['l'] # length of the dipoles
-    t = ticks(g, s, rs)[:-1] # left or bottom location of subareas
-    n = round(rd*rs**2*len(t)**2) # number of dislocations
-    # dislocation or dipole positions
-    if rv == 'D':
-        c = n//2 # 1 random point for 1 dipole of 2 dislocations
-    elif rv == 'R':
-        c = n # 1 random point for 1 dislocation
-    elif rv == 'E':
-        rf = round(r['d']*rs**2) # deduced filling
-        if rf%2 != 0:
-            raise ValueError("odd number of dislocations per cell")
-        c = rf * len(t)**2 # 1 random point for 1 dislocation
-    # choice of cells
-    if rv in ['R', 'D']:
-        x = 1.0*G.choice(t, size=c)
-        y = 1.0*G.choice(t, size=c)
-    elif rv == 'E':
-        x, y = even_positions(t, rf)
-    # choice of position in the cells
-    l1 = rt/2
-    l2 = rs - l1
-    m = G.integers(4, size=c)
-    rx = G.random(c)
-    ry = G.random(c)
-    np.add(x, l1 + rx*l2, x, where=m==0)
-    np.add(y,      ry*l1, y, where=m==0)
-    np.add(x,      rx*l2, x, where=m==1)
-    np.add(y, l2 + ry*l1, y, where=m==1)
-    np.add(x,      rx*l1, x, where=m==2)
-    np.add(y,      ry*l2, y, where=m==2)
-    np.add(x, l2 + rx*l1, x, where=m==3)
-    np.add(y, l1 + ry*l2, y, where=m==3)
-    # Burgers vector
-    if rv == 'R':
-        b = G.choice([1, -1], size=c)
-    elif rv == 'E':
-        b = even_senses(t, rf)
-    elif rv == 'D':
-        b = np.concatenate((np.ones(c), -np.ones(c)))
-        theta = 2*np.pi*G.random(c)
-        ux = rl/2 * np.cos(theta)
-        uy = rl/2 * np.sin(theta)
-        x = np.concatenate((x+ux, x-ux))
-        y = np.concatenate((y+uy, y-uy))
-    p = np.stack((x,y), axis=1)
+    t = ticks(g, s, r['s'])[:-1] # left or bottom location of subareas
+    nt = f * len(t)**2 # total number of dislocations
+    nh = nt//2 # half of the total number of dislocations
+    # dislocation positions
+    if r['v'] == 'D':
+        rp = G.random((nh, 2))
+    else:
+        rp = G.random((nt, 2))
+    l1 = r['t']/2 # wall brick length 1
+    l2 = r['s'] - l1 # wall brick length 2
+    m = G.integers(4, size=len(rp))
+    m2 = np.stack((m, m)).T
+    rp = np.where(m2==0, np.array([l1,  0])+np.array([l2, l1])*rp, rp)
+    rp = np.where(m2==1, np.array([ 0, l2])+np.array([l2, l1])*rp, rp)
+    rp = np.where(m2==2, np.array([ 0,  0])+np.array([l1, l2])*rp, rp)
+    rp = np.where(m2==3, np.array([l2, l1])+np.array([l1, l2])*rp, rp)
+    if r['v'] == 'D':
+        phi = 2*np.pi*G.random(nh)
+        ux = r['l']/2 * np.cos(phi)
+        uy = r['l']/2 * np.sin(phi)
+        ux = np.stack((ux, -ux), axis=1).ravel()
+        uy = np.stack((uy, -uy), axis=1).ravel()
+        rp = np.repeat(rp, 2, axis=0) + np.stack((ux, uy), axis=1)
+    p = even_positions(t, f) + rp
+    # dislocation Burgers vector
+    if r['v'] == 'R':
+        b = G.permutation(np.concatenate((np.ones(nh), -np.ones(nh))))
+    elif r['v'] == 'E':
+        b = even_senses(t, f)
+    elif r['v'] == 'D':
+        b = np.tile((1, -1), nh)
     # masking
     if g == 'circle':
         m = geometries.mask(g, s, p)
-        p = p[m]
-        b = b[m]
+        p, b = p[m], b[m]
     return p, b
